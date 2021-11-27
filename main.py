3761c6d026866b68c39bc51cba1d5b0c1ccf22f0
nimmy13 <hanthething@gmail.com>
2021-11-27 17:55:52 +0300

    adding banned and some other modifications

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import qrcode
 import os
-url = input('URL: ')
+def print_banner():
+    banner = '''
+  _   _ _______ ______ _____  __ ___  
+ | \ | |__   __|  ____|  __ \/_ |__ \ 
+ |  \| |  | |  | |__  | |__) || |  ) |
+ | . ` |  | |  |  __| |  ___/ | | / / 
+ | |\  |  | |  | |____| |     | |/ /_ 
+ |_| \_|  |_|  |______|_|     |_|____|  
+''' 
+    print(banner)
+
+
 def generate():
     img = qrcode.make(url)
     type(img)
     img.save('qr.png')
     print('QR Generated.')
+
+print_banner()
+
+url = input('QR Code Generator, please enter the URL: ')
 
 generate()
